fc0b39a454c38df19a55593022edfa62045bd900
Ricardo Garcia <richard2500ruiz@gmail.com>
2025-08-28 12:06:45 -0600

    Update sidebar.php
    Se modifica estructura y se agregan clases.

--- Codigo/app/views/layout/sidebar.php
+++ Codigo/app/views/layout/sidebar.php
@@ -1,27 +1,27 @@
 <!-- Sidebar -->
-    <div class="sidebar">
-        <h2>Menú</h2>
-        <a href="index.php?module=">Dashboard</a>
-        
-        <!-- Menu Generales -->
-        <div class="menu-item">
-            <a href="#">Generales</a>
-            <div class="submenu submenu-visible">
-                <a href="index.php?module=usuarios">&#x1F464; Usuarios</a> <!-- 👤 -->
-                <a href="index.php?module=clientes">&#x1F465; Clientes</a> <!-- 👥 -->
-                <a href="index.php?module=tiendas">&#x1F3EA; Tiendas</a> <!-- 🏪 -->
-            </div>
+<div class="sidebar">
+    <h1>MENÚ</h1>
+    <a href="index.php?module=" class="secciones">Dashboard</a>
+    
+    <!-- Menú Generales -->
+    <div class="menu-item">
+        <a href="#"  class="secciones">Generales</a>
+        <div class="submenu submenu-visible">
+            <a href="index.php?module=usuarios">&#x1F464; Usuarios</a>
+            <a href="index.php?module=clientes">&#x1F465; Clientes</a>
+            <a href="index.php?module=tiendas">&#x1F3EA; Tiendas</a>
         </div>
-
-    <!-- Menu Cotizaciones -->
-        <div class="menu-item">
-            <a href="#">Cotizaciones</a>
-            <div class="submenu submenu-visible">
-                <a href="index.php?module=cotizar">&#x270F;&#xFE0F; Cotizar</a> <!-- ✏️ -->
-                <a href="index.php?module=ver_cotizaciones">&#x1F4C3; Ver cotizaciones</a> <!-- 📃 -->
-                <a href="index.php?module=calendario">&#x1F4C5; Calendario de Cotizaciones</a> <!-- 📅 -->
-            </div>
-        </div>
-
-        <a href="index.php?module=facturacion">&#x1F4B3; Facturación</a> <!-- 💳 -->
     </div>
+
+    <!-- Menú Cotizaciones -->
+    <div class="menu-item">
+        <a href="#"  class="secciones">Cotizaciones</a>
+        <div class="submenu submenu-visible">
+            <a href="index.php?module=cotizar">&#x270F;&#xFE0F; Cotizar</a>
+            <a href="index.php?module=ver_cotizaciones">&#x1F4C3; Ver cotizaciones</a>
+            <a href="index.php?module=calendario">&#x1F4C5; Calendario de Cotizaciones</a>
+        </div>
+    </div>
+
+    <a href="index.php?module=facturacion"  class="secciones">&#x1F4B3; Facturación</a>
+</div>
