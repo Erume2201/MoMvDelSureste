08f55b4e77c0a976d31f2a63455884d84ae79cc3
Ricardo Garcia <richard2500ruiz@gmail.com>
2025-08-28 12:55:49 -0600

    Update sidebar.php y css
    Se agregan estilos, iconos, mejorar estructura, tipografía de sidebar.

--- Codigo/app/views/layout/sidebar.php
+++ Codigo/app/views/layout/sidebar.php
@@ -1,27 +1,62 @@
 <!-- Sidebar -->
 <div class="sidebar">
     <h1>MENÚ</h1>
-    <a href="index.php?module=" class="secciones">Dashboard</a>
-    
+
+    <!-- Dashboard -->
+    <a href="index.php?module=" class="secciones">
+        <i class='bx bxs-dashboard'></i>
+        Dashboard
+    </a>
+
     <!-- Menú Generales -->
     <div class="menu-item">
-        <a href="#"  class="secciones">Generales</a>
+        <a href="#" class="secciones submenu-toggle">
+            <i class='bx bxs-cog'></i>
+            Generales
+            <i class='bx bx-chevron-down arrow'></i>
+        </a>
         <div class="submenu submenu-visible">
-            <a href="index.php?module=usuarios">&#x1F464; Usuarios</a>
-            <a href="index.php?module=clientes">&#x1F465; Clientes</a>
-            <a href="index.php?module=tiendas">&#x1F3EA; Tiendas</a>
+            <a href="index.php?module=usuarios">
+                <i class='bx bxs-user'></i>
+                Usuarios
+            </a>
+            <a href="index.php?module=clientes">
+                <i class='bx bxs-group'></i>
+                Clientes
+            </a>
+            <a href="index.php?module=tiendas">
+                <i class='bx bxs-store'></i>
+                Tiendas
+            </a>
         </div>
     </div>
 
     <!-- Menú Cotizaciones -->
     <div class="menu-item">
-        <a href="#"  class="secciones">Cotizaciones</a>
+        <a href="#" class="secciones submenu-toggle">
+            <i class='bx bxs-briefcase'></i>
+            Cotizaciones
+            <i class='bx bx-chevron-down arrow'></i>
+        </a>
         <div class="submenu submenu-visible">
-            <a href="index.php?module=cotizar">&#x270F;&#xFE0F; Cotizar</a>
-            <a href="index.php?module=ver_cotizaciones">&#x1F4C3; Ver cotizaciones</a>
-            <a href="index.php?module=calendario">&#x1F4C5; Calendario de Cotizaciones</a>
+            <a href="index.php?module=cotizar">
+                <i class='bx bxs-edit-alt'></i>
+                Cotizar
+            </a>
+            <a href="index.php?module=ver_cotizaciones">
+                <i class='bx bxs-file'></i>
+                Ver cotizaciones
+            </a>
+            <a href="index.php?module=calendario">
+                <i class='bx bxs-calendar'></i>
+                Calendario de Cotizaciones
+            </a>
         </div>
     </div>
 
-    <a href="index.php?module=facturacion"  class="secciones">&#x1F4B3; Facturación</a>
+    <!-- Facturación -->
+    <a href="index.php?module=facturacion" class="secciones">
+        <i class='bx bxs-credit-card'></i>
+        Facturación
+    </a>
 </div>
