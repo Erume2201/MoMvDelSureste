a22d5a8dc831331c565410bfc5dd549b23f72c66
Ricardo Garcia <richard2500ruiz@gmail.com>
2025-08-27 23:23:10 -0600

    Update dashboard.php
    Se incluyen el archivo sidebar.php.
    Se agrega la ruta css del sidebar.
    Se elimina el menú en código en dashboard. Se deja limpio.

--- Codigo/app/views/dashboard.php
+++ Codigo/app/views/dashboard.php
@@ -1,7 +1,9 @@
-<!--  -->
+<!-- Se incluyen archivos php -->
 <?php 
 require_once __DIR__ . '/../config/config.php'; 
+include __DIR__ . '../layout/sidebar.php'; // Esto solo carga el sidebar
 ?>
+
 
 <!DOCTYPE html>
 <html lang="es">
@@ -11,38 +13,11 @@ require_once __DIR__ . '/../config/config.php';
     <!--icono en la pestaña-->
     <link rel="icon" href="public/img/favicon.ico" type="image/x-icon">
     <!-- Llamando los estilos con la BASE_URL -->
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/sidebar.css">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/dashboard.css">
     <!-- <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/estilos.css"> -->
 </head>
 <body>
-    <!-- Sidebar -->
-    <div class="sidebar">
-        <h2>Menú</h2>
-        <a href="index.php?module=">Dashboard</a>
-        <!-- Menu Generales -->
-        <div class="menu-item">
-            <a href="#">Generales</a>
-            <div class="submenu submenu-visible">
-                <a href="index.php?module=usuarios">&#x1F464; Usuarios</a> <!-- 👤 -->
-                <a href="index.php?module=clientes">&#x1F465; Clientes</a> <!-- 👥 -->
-                <a href="index.php?module=tiendas">&#x1F3EA; Tiendas</a> <!-- 🏪 -->
-            </div>
-        </div>
-
-    <!-- Menu Cotizaciones -->
-        <div class="menu-item">
-            <a href="#">Cotizaciones</a>
-            <div class="submenu submenu-visible">
-                <a href="index.php?module=cotizar">&#x270F;&#xFE0F; Cotizar</a> <!-- ✏️ -->
-                <a href="index.php?module=ver_cotizaciones">&#x1F4C3; Ver cotizaciones</a> <!-- 📃 -->
-                <a href="index.php?module=calendario">&#x1F4C5; Calendario de Cotizaciones</a> <!-- 📅 -->
-            </div>
-        </div>
-
-        <a href="index.php?module=facturacion">&#x1F4B3; Facturación</a> <!-- 💳 -->
-    </div>
-
-
    <div class="main-content">
         <header>
             <h1>Dashboard, Bienvenido <?= htmlspecialchars($_SESSION['usuario']); ?></h1>
