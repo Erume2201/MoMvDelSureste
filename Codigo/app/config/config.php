646e24a036954b3736e49724cf2e4bac7af75095
Erume2201 <135057750+Erume2201@users.noreply.github.com>
2025-09-06 17:30:31 -0600

    Update config.php
    cambio del dominio

--- Codigo/app/config/config.php
+++ Codigo/app/config/config.php
@@ -2,7 +2,7 @@
 // app/config/config.php
 
 // URL base del proyecto
-define('BASE_URL', 'http://localhost/MoMvDelSureste/Codigo/');
+define('BASE_URL', 'https://mo-mv-del-sureste.vercel.app//MoMvDelSureste/Codigo/');
 
 
 
