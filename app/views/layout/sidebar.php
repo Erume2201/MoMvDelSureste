a839ee97a3bfc2f80c06558cc1030e3ebf8017af
Erume2201 <135057750+Erume2201@users.noreply.github.com>
2025-09-11 22:07:22 -0600

    modulos

--- app/views/layout/sidebar.php
+++ app/views/layout/sidebar.php
@@ -38,6 +38,14 @@
                 <i class='bx bxs-store'></i>
                 Tiendas
             </a>
+            <a href="<?php echo BASE_URL; ?>index.php?module=Operador" class="submenu-link" data-module="Operador">
+                <i class='bx bxs-store'></i>
+                Operadores
+            </a>
+            <a href="<?php echo BASE_URL; ?>index.php?module=Unidades" class="submenu-link" data-module="Unidades">
+                <i class='bx bxs-store'></i>
+                Unidades
+            </a>
         </div>
     </div>
 
@@ -57,6 +65,18 @@
                 <i class='bx bxs-file'></i>
                 Visualizar contratos
             </a>
+            
+        </div>
+    </div>
+
+    <!-- Menú Viajes -->
+    <div class="menu-item" data-group="Viajes">
+        <a href="javascript:void(0);" class="secciones submenu-toggle" data-group="Viajes-toggle" aria-expanded="false">
+            <i class='bx bxs-briefcase'></i>
+            Viajes
+            <i class='bx bx-chevron-down arrow'></i>
+        </a>
+        <div class="submenu" id="submenu-Viajes">
             <a href="<?php echo BASE_URL; ?>index.php?module=calendario" class="submenu-link" data-module="calendario">
                 <i class='bx bxs-calendar'></i>
                 Calendario de recolección
@@ -65,8 +85,11 @@
                 <i class='bx bxs-calendar'></i>
                 Generar Manifiesto
             </a>
+            
         </div>
     </div>
+
+   
 
     <!-- Facturación -->
     <div class="menu-item" data-group="facturacion">
